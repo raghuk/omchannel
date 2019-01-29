d245fdba7fc30de1cb7b0d45e328823371564869
Raghu Kumar <raghuk@users.noreply.github.com>
2019-01-29 15:20:38 +0530

    updated player component
    - Youtube Videos to play in browser for Android

--- src/components/player/index.js
+++ src/components/player/index.js
@@ -1,11 +1,21 @@
 import React, { PureComponent } from 'react';
-import { View, WebView } from 'react-native';
-import { ScreenOrientation } from 'expo';
+import { Platform, View, WebView } from 'react-native';
+import { ScreenOrientation, WebBrowser } from 'expo';
+import { Button } from 'react-native-elements';
 
 import styles from './styles';
 
 
 class Player extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    const { navigation } = this.props;
+    const video = navigation.state.params.video;
+
+    this.url = `https://www.youtube.com/embed/${video.videoId}`;
+  }
+
   componentDidMount() {
     ScreenOrientation.allowAsync(ScreenOrientation.Orientation.ALL);
   }
@@ -14,16 +24,33 @@ class Player extends PureComponent {
     ScreenOrientation.allowAsync(ScreenOrientation.Orientation.PORTRAIT);
   }
 
-  render() {
-    const { navigation } = this.props;
+  handleLaunch = () => {
+    WebBrowser.openBrowserAsync(this.url);
+  }
 
-    const video = navigation.state.params.video;
-    const url = `https://www.youtube.com/embed/${video.videoId}`;
+  render() {
+    if (Platform.OS === 'android') {
+      return (
+        <View style={styles.content}>
+          <Button
+            raised
+            containerViewStyle={{ width: '85%', paddingLeft: 20, paddingRight: 15, backgroundColor: '#8AC24A', borderRadius: 5 }}
+            textStyle={{ textAlign: 'center' }}
+            backgroundColor="#8AC24A"
+            fontSize={20}
+            fontFamily="Titillium"
+            icon={{ name: 'logo-youtube', size: 32, type: 'ionicon' }}
+            title="Please click here to watch the video."
+            onPress={() => this.handleLaunch()}
+          />
+        </View>
+      );
+    }
 
     return (
       <View style={styles.content}>
         <WebView
-          source={{ uri: url }}
+          source={{ uri: this.url }}
           startInLoadingState
           scalesPageToFit
           javaScriptEnabled
